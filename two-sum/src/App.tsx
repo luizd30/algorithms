8c76a41d107e0cbead1ad558bdb050ecc0294e76
Luiz Eduardo Dornelas <luizd30@gmail.com>
2024-05-21 10:27:17 -0300

    feat: add a inputs to user interaction

--- two-sum/src/App.tsx
+++ two-sum/src/App.tsx
@@ -1,37 +1,63 @@
+import { useForm } from "react-hook-form";
 import "./App.css";
 import { bruteForce, hashmap } from "./utils/twoSum";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
+const schema = z.object({
+  target: z
+    .string()
+    .min(1, { message: "Must be at least 1 character." })
+    .regex(/^\d+$/, { message: "Must contain only numbers." }),
+  numbers: z.string().regex(/^\d+(,\d+)*$/, {
+    message: "Must contain numbers separated by commas without spaces.",
+  }),
+});
+
+type SchemaType = z.infer<typeof schema>;
+
+type InputType = { bruteforce: number[] | []; hash: number[] | [] };
 
 function App() {
-  const arrayOfNumbers = [
-    {
-      nums: [2, 7, 11, 15],
-      target: 9,
-    },
-    {
-      nums: [3, 2, 4],
-      target: 6,
-    },
-    {
-      nums: [3, 3],
-      target: 6,
-    },
-  ];
+  const [result, setResult] = useState<InputType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SchemaType>({ resolver: zodResolver(schema) });
+
+  const calculate = (data: SchemaType) => {
+    const { target, numbers } = data;
+
+    const targetToNumber = Number(target);
+    const numbersToNumber = numbers.split(",").map((number) => Number(number));
+
+    const bruteForceResult = bruteForce(numbersToNumber, targetToNumber);
+    const hashmapResult = hashmap(numbersToNumber, targetToNumber);
+
+    setResult({ bruteforce: bruteForceResult, hash: hashmapResult });
+  };
 
   return (
     <>
-      <h2>Brute Force</h2>
-      {arrayOfNumbers.map(({ nums, target }, index) => (
-        <h3 key={index}>
-          {JSON.stringify(nums)} = {bruteForce(nums, target).join(", ")}
-        </h3>
-      ))}
+      <h2>Two Sum</h2>
+      <form onSubmit={handleSubmit(calculate)}>
+        <label htmlFor="numbers">Numbers</label>
+        <input type="text" id="numbers" {...register("numbers")} />
+        {errors.numbers && <p>{errors.numbers.message}</p>}
+        <label htmlFor="target">Target</label>
+        <input type="text" id="target" {...register("target")} />
+        {errors.target && <p>{errors.target.message}</p>}
+        <button type="submit">calc</button>
+      </form>
 
-      <h2>Hash Map</h2>
-      {arrayOfNumbers.map(({ nums, target }, index) => (
-        <h3 key={index}>
-          {JSON.stringify(nums)} = {hashmap(nums, target).join(", ")}
-        </h3>
-      ))}
+      <h3>Brute Force Result: </h3>
+      {result?.bruteforce.length
+        ? JSON.stringify(result.bruteforce)
+        : "invalid"}
+      <h3>Hash Map Result: </h3>
+      {result?.hash.length ? JSON.stringify(result.hash) : "invalid"}
     </>
   );
 }
